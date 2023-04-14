05b03361d34ffab610f911e8621ecaf055b3a444
menaBM <mena.bousfieldmilton@sky.uk>
2023-04-14 12:07:12 +0100

    /jokes route is working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const { Joke } = require('./db');
+const { Joke, Sequelize } = require('./db');
+const Op = Sequelize.Op
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -8,7 +9,23 @@ app.use(express.urlencoded({extended:true}));
 app.get('/jokes', async (req, res, next) => {
   try {
     // TODO - filter the jokes by tags and content
-    const jokes = [];
+    let jokes = [];
+    
+    if (req.query.tags){
+      jokes = await Joke.findAll({where: {
+        tags:{
+          [Op.substring]: req.query.tags
+        }}
+      })
+    }else if(req.query.content){
+      jokes = await Joke.findAll({where: {
+        joke: {
+          [Op.substring]: req.query.content
+        }}})
+    }else{
+    jokes = await Joke.findAll()
+  }
+
     res.send(jokes);
   } catch (error) {
     console.error(error);
